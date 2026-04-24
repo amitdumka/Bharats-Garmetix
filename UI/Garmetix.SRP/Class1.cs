a4595e2dace41a10a71976ccf0f24af5d9ecf24e
Amit Kumar <amitnarayansah@outlook.com>
2026-04-24 19:39:12 +0530

    srp route

---
+++ UI/Garmetix.SRP/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace Garmetix.SRP
+{
+    // All the code in this file is included in all platforms.
+    public static class GarmetixSRPUIModule
+    {
+        public static MauiAppBuilder ConfigureGarmetixSRPUI(this MauiAppBuilder builder)
+        {
+            // Register services, handlers, etc. here
+            // For example:
+            // builder.Services.AddSingleton<IMyService, MyServiceImplementation>();
+            return builder;
+        }
+
+
+        public static bool EnableSRPUIRoutes()
+        {
+            // Enable routing for SRP UI components
+            return true;
+        }
+
+
+
+        public static MauiAppBuilder UseModules(this MauiAppBuilder builder) { 
+        
+            // 
+            return builder;
+        
+        }
+    }
+}
